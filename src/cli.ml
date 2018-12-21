33cc63dc54093833902855e19c333802772d2246
Daniel Perez <daniel@perez.sh>
2018-12-21 12:59:48 +0100

    Improve CLI

--- src/cli.ml
+++ src/cli.ml
@@ -5,13 +5,26 @@ let show_flashcard_command =
   Command.basic
   ~summary:"Shows a single flashcard. Only one of the three options must be provided"
   [%map_open
-    let translation = flag "-translation" (optional string) ~doc:"the translation of the card to show"
-    and word = flag "-word" (optional string) ~doc:"the word of the card to show"
-    and random = flag "-random" no_arg ~doc:"shows a random card" in
-    fun () -> Commands.show_command translation word random
+    let choice = choose_one ~if_nothing_chosen:`Raise
+      [ flag "-translation" (optional string)
+        ~aliases:["-t"] ~doc:"the translation of the card to show"
+      |> map ~f:(Option.map ~f:(fun v -> `Translation v))
+      ; flag "-word" (optional string)
+        ~aliases:["-w"] ~doc:"the word of the card to show"
+      |> map ~f:(Option.map ~f:(fun v -> `Word v))
+      ; flag "-random" no_arg
+        ~aliases:["-r"] ~doc:"shows a random card"
+      |> map ~f:(fun v -> Option.some_if v `Random)
+    ]
+    in
+    fun () -> Commands.show_command choice
   ]
 
+let flashcards_command =
+  Command.group ~summary:"CLI based flashcards" [
+    ("show", show_flashcard_command)
+  ]
 
 let run () =
   Random.self_init ();
-  Command.run ~build_info:"" ~version:"0.1" show_flashcard_command
+  Command.run ~build_info:"" ~version:"0.1" flashcards_command
--- src/commands.ml
+++ src/commands.ml
@@ -6,10 +6,9 @@ let show_record opt_record = match opt_record with
   | Some record -> Out_channel.print_endline (Vocabulary.Record.format record)
   | None -> Out_channel.prerr_endline "no record found"
 
-let show_command translation word random =
+let show_command choice =
   let vocabulary = Vocabulary.from_file vocab_path (module MarkdownTable.Parser) in
-  match (translation, word, random) with
-  | Some tr, None, false -> show_record (Vocabulary.search_by_translation vocabulary tr)
-  | None, Some w, false -> show_record (Vocabulary.search_by_word vocabulary w)
-  | None, None, true -> show_record (Vocabulary.random_record vocabulary)
-  | _ -> Out_channel.prerr_endline "only one of -translation, -word or -random should be passed"
+  match choice with
+  | `Translation tr -> show_record (Vocabulary.search_by_translation vocabulary tr)
+  | `Word w -> show_record (Vocabulary.search_by_word vocabulary w)
+  | `Random -> show_record (Vocabulary.random_record vocabulary)
